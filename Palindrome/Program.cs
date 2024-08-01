2ca0066031657b4ae2e4887c9cfc2c57857ef1d1
Kamelia Doynova <kameliadoynova@abv.bg>
2024-08-01 15:45:50 +0300

    Palindrome task
    Find out if the word is a palindrome.

---
+++ Palindrome/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+while (true)
+{
+    Console.Write("Enter word: ");
+
+    string word = Console.ReadLine();
+
+    bool isPalindrome = false;
+    int j = word.Length - 1;
+
+
+    for (int i = 0; i < word.Length; i++)
+    {
+        if (word[i] == word[j])
+        {
+            isPalindrome = true;
+        }
+        else
+        {
+            isPalindrome = false;
+            break;
+        }
+
+        j--;
+    }
+
+
+    if (isPalindrome)
+        Console.Write("It's Palendorm.");
+    else
+        Console.Write("It's not Palendorm.");
+
+    Console.WriteLine(string.Join(Environment.NewLine, Enumerable.Repeat(string.Empty, 2)));
+}
